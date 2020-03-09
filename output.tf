0c9169547f32cc8eba9f60d25e7fdae363b26a23
Otabek Samatov <otabeksamatov11235@gmail.com>
2020-03-09 10:37:57 -0500

    updated outputs

--- output.tf
+++ output.tf
@@ -5,3 +5,20 @@ output "IP" {
 output "KEY" {
   value = "${module.wordpress.KEY}"
 }
+
+
+output "Bucketname" {
+  value = "${module.wordpress.KEY}"
+}
+
+output "AMI" {
+  value = "${module.wordpress.KEY}"
+}
+
+output "ROUTE53" {
+  value = "${module.wordpress.KEY}"
+}
+
+output "SEC_GROUP" {
+  value = "${module.wordpress.KEY}"
+}
