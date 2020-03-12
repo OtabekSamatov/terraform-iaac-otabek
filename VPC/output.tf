33d66e60fa909e225a235ec784e99d4cc95d6f49
Otabek Samatov <otabeksamatov11235@gmail.com>
2020-03-12 18:38:58 -0500

    vpc output

--- VPC/output.tf
+++ VPC/output.tf
@@ -1,3 +1,31 @@
 output "VPC_ID" {
     value = "${module.dev.VPC_ID}"
 }
+
+output "Subnet1" {
+    value = "$module.dev.subnet1}"
+}
+
+output "Subnet2" {
+    value = "$module.dev.subnet2}"
+}
+
+output "Subnet3" {
+    value = "$module.dev.subnet3}"
+}
+
+output "Private Subnet1" {
+    value = "$module.dev.Private_subnet1}"
+}
+
+output "Private Subnet2" {
+    value = "$module.dev.Private_subnet2}"
+}
+
+output "Private Subnet1" {
+    value = "$module.dev.Private_subnet3}"
+}
+
+output "IGW" {
+  value       =  "${aws_internet_gateway.dev.id}"  
+}
