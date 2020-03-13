19e51490a7b9d54a2b08034c9a0f69113251a22f
Otabek Samatov <otabeksamatov11235@gmail.com>
2020-03-13 15:23:24 -0500

    updated

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -1,7 +1,10 @@
+# Create a new instance of the latest Ubuntu 14.04 on an
+# t2.micro node with an AWS Tag naming it "HelloWorld"
 provider "aws" {
   region = "us-west-2"
 }
-# Gets Ubuntu AMI
+
+#Gets ubunt AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -15,8 +18,10 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  owners = ["099720109477"] # Canonical
+  owners = ["099720109477"] # Canonical #This owner never changes
 }
+
+
 
 output "UBUNTU_AMI_ID" {
   value = "${data.aws_ami.ubuntu.id}"
@@ -24,14 +29,13 @@ output "UBUNTU_AMI_ID" {
 
 
 
-
-# Gets Centos AMI
-data "aws_ami" "centos" {
+#Gets  CentOS AMI
+data "aws_ami" "centos" { #should be uniq
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["CentOS Linux 7 x86_64 HVM EBS ENA 1901_01-b7*"]
+    values = ["CentOS Linux 7 x86_64 HVM EBS *"]
   }
 
   filter {
@@ -39,11 +43,13 @@ data "aws_ami" "centos" {
     values = ["hvm"]
   }
 
-  owners = ["679593333241"] # Canonical
+  owners = ["679593333241"] # Canonical #This owner never changes
 }
 
+
+
 output "CENTOS_AMI_ID" {
-  value = "${data.aws_ami.ubuntu.id}"
+  value = "${data.aws_ami.centos.id}"
 }
 
 resource "aws_key_pair" "provisioner" {
@@ -51,29 +57,34 @@ resource "aws_key_pair" "provisioner" {
   public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
 
+
+
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.centos.id}"
+  ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.provisioner.key_name}"
-  provisioner "remote-exec" {
-    connection {
-        type     = "ssh"
-        user     = "centos"
-        private_key = "${file("~/.ssh/id_rsa")}"
-        host     = "${self.public_ip}"
+
+# Copies the file as the root user using SSH
+provisioner "remote-exec" {
+
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = "${file("~/.ssh/id_rsa")}"
+    host     = "${self.public_ip}"
     }
 
+     inline= [
+       " sudo apt-get install telnet -y",
+       "sudo mkdir /tmp/ubuntu", 
+       "w",
+       "free -m",
+       "sleep 5"
+       ]
 
-        inline = [
-           "sudo yum install telnet -y",
-           "sudo mkdir /tmp/centos",
-           "w",
-           "free -m",
-           "sleep 5"
-        ]
+  }
 
-    }
-    
+   
 
   tags = {
     Name = "HelloWorld"
