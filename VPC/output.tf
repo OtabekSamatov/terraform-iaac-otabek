df041f88680d41391b3e6dbc47762b753487d036
Otabek Samatov <otabeksamatov11235@gmail.com>
2020-03-14 14:17:14 -0500

    +

--- VPC/output.tf
+++ VPC/output.tf
@@ -3,27 +3,27 @@ output "VPC_ID" {
 }
 
 output "Subnet1" {
-    value = "${module.dev.subnet1}"
+    value = "${module.dev.Subnet1}"
 }
 
 output "Subnet2" {
-    value = "${module.dev.subnet2}"
+    value = "${module.dev.Subnet2}"
 }
 
 output "Subnet3" {
-    value = "${module.dev.subnet3}"
+    value = "${module.dev.Subnet3}"
 }
 
 output "Private_Subnet1" {
-    value = "${module.dev.Private_subnet1}"
+    value = "${module.dev.Private_Subnet1}"
 }
 
 output "Private_Subnet2" {
-    value = "${module.dev.Private_subnet2}"
+    value = "${module.dev.Private_Subnet2}"
 }
 
 output "Private_Subnet3" {
-    value = "${module.dev.Private_subnet3}"
+    value = "${module.dev.Private_Subnet3}"
 }
 
 output "IGW" {
