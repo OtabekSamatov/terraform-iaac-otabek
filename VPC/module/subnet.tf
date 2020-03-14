8d5da8eb498a3f8ad5a044a82ecf060a7399e9d9
Otabek Samatov <otabeksamatov11235@gmail.com>
2020-03-14 14:28:03 -0500

    +

--- VPC/module/subnet.tf
+++ VPC/module/subnet.tf
@@ -4,19 +4,19 @@
 resource "aws_subnet" "dev1" { 
   vpc_id = "${aws_vpc.dev.id}" 
   cidr_block = "${var.cidr_block1_public}" 
-  availability_zone = "us-east-1a"
+  availability_zone = "${var.public_subnet1}"
 } 
 resource "aws_subnet" "dev2" { 
   vpc_id = "${aws_vpc.dev.id}" 
   cidr_block = "${var.cidr_block2_public}" 
-  availability_zone = "us-east-1b"
+  availability_zone = "${var.public_subnet2}"
 
 } 
 
 resource "aws_subnet" "dev3" { 
   vpc_id = "${aws_vpc.dev.id}" 
   cidr_block = "${var.cidr_block3_public}"
-  availability_zone = "us-east-1c"
+  availability_zone = "${var.public_subnet3}"
 
 } 
 
--- VPC/module/variable.tf
+++ VPC/module/variable.tf
@@ -2,10 +2,18 @@ variable "region" {}
 
 variable "cidr_block" {}
 
+
 variable "cidr_block1_public" {}
 variable "cidr_block2_public" {}
 variable "cidr_block3_public" {}
 
+
 variable "cidr_block1_private" {}
 variable "cidr_block2_private" {}
 variable "cidr_block3_private" {}
+
+
+variable "public_subnet1" {}
+variable "public_subnet2" {}
+variable "public_subnet3" {}
+
